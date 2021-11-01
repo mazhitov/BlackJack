5bc10fffb2838fe6d6f6037f6442e3f46045a8ab
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-01 13:51:52 +0600

    add rank, suit and suitSymbol to card.html

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+  @Input() rank = '';
+  @Input() suit = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getSymbol() {
+    switch (this.suit) {
+      case 'diams':
+        return '♦'
+        break;
+      case 'hearts':
+        return '♥';
+        break;
+      case 'clubs':
+        return '♣';
+        break;
+      case 'spades':
+        return '♠';
+        break;
+      default:
+        console.log('there is no symbol');
+    }
   }
-
+  getClassName() {
+    return `card rank-${this.rank} ${this.suit}`;
+  }
 }
